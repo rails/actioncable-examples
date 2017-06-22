61c12b8e48b057ab7d44363f0000f673cfe401c5
Julia López <julialopez@gmail.com>
2017-06-22 14:46:44 -0500

    specify rails version in migrations

--- db/migrate/20150711103112_create_comments.rb
+++ db/migrate/20150711103112_create_comments.rb
@@ -1,8 +1,8 @@
-class CreateComments < ActiveRecord::Migration
+class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.references :message, index: true, foreign_key: true
-      t.references :user, index: true, foreign_key: true
+      t.references :message, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.text :content
 
