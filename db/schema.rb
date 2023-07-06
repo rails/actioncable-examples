522a2b9c08e4757906b0d2668a6a89c8568e5ff8
julianfssen <julianfssen@gmail.com>
2023-07-07 01:42:25 +0800

    Added migration to create User

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_173658) do
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
