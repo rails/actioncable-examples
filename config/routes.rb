76ff93625944c28df433ba7fdd96c9eac8d32fae
julianfssen <julianfssen@gmail.com>
2023-07-07 01:57:50 +0800

    Add Example routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  Rails.application.routes.draw do
+    root "examples#index"
+  end
 end
