b7c3d4a32f7bebe5d22368cd550b18fff5145ace
julianfssen <julianfssen@gmail.com>
2023-07-07 01:58:27 +0800

    Rename app references to actioncable_examples

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TestApp
+module ActioncableExamples
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
