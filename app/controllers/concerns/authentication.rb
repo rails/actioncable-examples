cbe6726f14b41ac7cff98972b616fa90e7b5a436
julianfssen <julianfssen@gmail.com>
2023-07-07 10:02:55 +0800

    fixup! Add sessions to ExamplesController

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,12 +1,18 @@
 module Authentication
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :ensure_authenticated_user
+  end
+
   private
     def ensure_authenticated_user
-      authenticate_user(cookies.signed[:user_id]) || redirect_to(new_session_url)
+      authenticate_user(cookies.encrypted[:user_id]) || redirect_to(new_session_url)
     end
 
     def authenticate_user(user_id)
       if authenticated_user = User.find_by(id: user_id)
-        cookies.signed[:user_id] ||= user_id
+        cookies.encrypted[:user_id] ||= user_id
         @current_user = authenticated_user
       end
     end
